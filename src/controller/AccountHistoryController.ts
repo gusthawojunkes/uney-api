12962ff331832e3f09e212ab30835c9876d5f30d
Gusthawo Junkes <gusthawojunkes14@gmail.com>
2021-08-28 19:59:26 -0300

    Removing a register

--- src/controller/AccountHistoryController.ts
+++ src/controller/AccountHistoryController.ts
@@ -3,38 +3,49 @@ import { AccountHistory } from '../entity/AccountHistory';
 import { Account } from '../entity/Account';
 import { Request, Response } from 'express';
 import { TransactionType } from '../entity/AccountHistory';
+import { copyFileSync } from 'fs';
 
 export const saveHistoric = async (request: Request, response: Response) => {
     const accId: number = request.body.accountId;
     const account: Account = await getRepository(Account).findOne(accId);
-    const newHistoric: AccountHistory = getNewHistoricFromBody(request.body, account);
+    const newHistoric: AccountHistory = getNewHistoricFromBody(
+        request.body,
+        account
+    );
 
     const value: number = newHistoric.value;
     const operation: TransactionType = newHistoric.operation;
 
     updateBalance(account, value, operation)
-    .then(() => {
-        getRepository(AccountHistory).save(newHistoric)
-        .then(historic => { 
-            return response.json(historic);
+        .then(() => {
+            getRepository(AccountHistory)
+                .save(newHistoric)
+                .then((historic) => {
+                    return response.json(historic);
+                });
         })
-    })
-    .catch(err => { return response.status(500).send({ error: err.message }); });
-    
+        .catch((err) => {
+            return response.status(500).send({ error: err.message });
+        });
 };
 
 export const getHistoric = async (_request: Request, response: Response) => {
-    const historic: Array<AccountHistory> = await getRepository(AccountHistory).find({
+    const historic: Array<AccountHistory> = await getRepository(
+        AccountHistory
+    ).find({
         order: {
-            created_at: 'DESC'
-        }
+            created_at: 'DESC',
+        },
     });
     return response.json(historic);
 };
 
 export const updateHistoric = async (request: Request, response: Response) => {
     const { id } = request.params;
-    const historic: UpdateResult = await getRepository(AccountHistory).update(id, request.body);
+    const historic: UpdateResult = await getRepository(AccountHistory).update(
+        id,
+        request.body
+    );
     if (historic.affected === 1) {
         const updated = await getRepository(AccountHistory).findOne(id);
         return response.json(updated);
@@ -44,22 +55,34 @@ export const updateHistoric = async (request: Request, response: Response) => {
 
 export const deleteHistoric = async (request: Request, response: Response) => {
     const { id } = request.params;
-    const historic: DeleteResult = await getRepository(AccountHistory).delete(id);
-    if (historic.affected === 1) {
-        return response.json({ message: `Register ${id} removed from database` });
+    const accId: number = request.body.accountId;
+    const account: Account = await getRepository(Account).findOne(accId);
+    const historic = await getRepository(AccountHistory).findOne(id);
+    console.log(historic);
+    cleanValuesFromAccount(account, historic);
+    const register: DeleteResult = await getRepository(AccountHistory).delete(
+        id
+    );
+    if (register.affected === 1) {
+        return response.json(account.balance);
     }
     return response.status(404).json({ message: 'Register not found' });
 };
 
 export const markAsFavorite = async (request: Request, response: Response) => {
     const { id } = request.params;
-    const historic: UpdateResult = await getRepository(AccountHistory).update(id, request.body);
+    const historic: UpdateResult = await getRepository(AccountHistory).update(
+        id,
+        request.body
+    );
     if (historic.affected === 1) {
-        const historic: AccountHistory = await getRepository(AccountHistory).findOne(id);
+        const historic: AccountHistory = await getRepository(
+            AccountHistory
+        ).findOne(id);
         return response.json(historic);
     }
     return response.status(404).json({ message: 'Register not found' });
-}
+};
 
 const getNewHistoricFromBody = (body: any, acc: Account): AccountHistory => {
     const historic = new AccountHistory();
@@ -70,13 +93,33 @@ const getNewHistoricFromBody = (body: any, acc: Account): AccountHistory => {
     return historic;
 };
 
-const updateBalance = async (account: Account, value: number, type: TransactionType) => {
+const updateBalance = async (
+    account: Account,
+    value: number,
+    type: TransactionType
+) => {
     if (type === 'credit') {
         account.balance += value;
         account.total_input += value;
     } else if (type === 'debit') {
         account.balance -= value;
         account.total_output += value;
+    }
+    return await getRepository(Account).update(account.id, account);
+};
+
+const cleanValuesFromAccount = async (
+    account: Account,
+    historic: AccountHistory
+) => {
+    const type: TransactionType = historic.operation;
+    const value: number = historic.value;
+    if (type === 'credit') {
+        account.balance -= value;
+        account.total_input -= value;
+    } else if (type === 'debit') {
+        account.balance += value;
+        account.total_output -= value;
     }
     return await getRepository(Account).update(account.id, account);
 };
