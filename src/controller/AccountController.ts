cd03f89473f6ed44a1b47f2204346bdbf2e7d9a2
Gusthawo Junkes <gusthawojunkes14@gmail.com>
2021-09-04 15:39:49 -0300

    Feature totals

--- src/controller/AccountController.ts
+++ src/controller/AccountController.ts
@@ -1,4 +1,10 @@
-import { getRepository, getConnection } from 'typeorm';
+import {
+    getRepository,
+    getConnection,
+    Repository,
+    Connection,
+    SelectQueryBuilder,
+} from 'typeorm';
 import { Account } from '../entity/Account';
 import { Request, Response } from 'express';
 import { AccountHistory } from '../entity/AccountHistory';
@@ -12,44 +18,60 @@ export const getBalance = async (request: Request, response: Response) => {
 };
 
 export const getTotals = async (request: Request, response: Response) => {
-    const accountId: number = Number(request.params);
+    const accountId: number = Number(request.params.id);
     let totals: AccountTotals = new AccountTotals();
-    console.log(totals);
-    const accountRepository = getRepository(Account);
-    accountRepository
-        .findOne(accountId)
-        .then((account) => {
-            totals.totalInput = account.total_input;
-            totals.totalOutput = account.total_output;
-        })
-        .catch(() => {});
-    calculateTotalToday('credit', accountId)
-        .then((value) => {
-            totals.totalInputToday = value !== undefined ? value : 0;
-        })
-        .catch(() => (totals.totalInputToday = 0));
-    calculateTotalToday('debit', accountId)
-        .then((value) => {
-            totals.totalOutputToday = value !== undefined ? value : 0;
-        })
-        .catch(() => (totals.totalOutputToday = 0));
 
-    return response.json({
-        totals,
-    });
+    totals = await setAccountTotals(totals, accountId);
+    totals = await setTotalsToday(totals, accountId);
+
+    return response.json(totals);
 };
 
 const calculateTotalToday = async (
     type: TransactionType,
     accountId: number
 ): Promise<number> => {
-    const total: any = await getConnection()
-        .createQueryBuilder()
-        .select('SUM(historic.value)')
-        .from(AccountHistory, 'historic')
-        .where('historic.operation = :operation', { operation: type })
-        .andWhere('historic.accountId = :accId', { accId: accountId })
-        .andWhere('historic.created_at::DATE = CURRENT_DATE')
-        .getRawOne();
-    return Number(total);
+    const databaseConnection: Connection = getConnection();
+    let totalToday: number = 0;
+    try {
+        const totalTodayQuery: SelectQueryBuilder<AccountHistory> =
+            databaseConnection
+                .createQueryBuilder()
+                .select('SUM(historic.value) AS total')
+                .from(AccountHistory, 'historic')
+                .where('historic.operation = :operation', { operation: type })
+                .andWhere('historic.accountId = :accountId', {
+                    accountId: accountId,
+                })
+                .andWhere('historic.created_at::DATE = CURRENT_DATE');
+
+        const { total } = await totalTodayQuery.getRawOne();
+        totalToday = total;
+    } catch (error) {
+        console.error(error);
+    }
+
+    return totalToday;
+};
+
+const setTotalsToday = async (
+    totals: AccountTotals,
+    accountId: number
+): Promise<AccountTotals> => {
+    const totalInputToday = await calculateTotalToday('credit', accountId);
+    const totalOutputToday = await calculateTotalToday('debit', accountId);
+    totals.setTotalInputToday(totalInputToday);
+    totals.setTotalOutputToday(totalOutputToday);
+    return totals;
+};
+
+const setAccountTotals = async (
+    totals: AccountTotals,
+    accountId: number
+): Promise<AccountTotals> => {
+    const accountRepository: Repository<Account> = getRepository(Account);
+    const account: Account = await accountRepository.findOne(accountId);
+    totals.setTotalInput(account.total_input);
+    totals.setTotalOutput(account.total_output);
+    return totals;
 };
